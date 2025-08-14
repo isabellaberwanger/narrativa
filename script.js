98abb4694375ead1b309b2e2f853cd32ad2ddd64
mitinhobr <141642157+isabellaberwanger@users.noreply.github.com>
2025-08-14 12:16:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,67 +6,67 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+        enunciado: "Você é uma aluna nova em um colégio. Você precisa achar sua sala de aula. Você:",
         alternativas: [
             {
-                texto: "Isso é assustador!",
-                afirmacao: "No início ficou com medo do que essa tecnologia pode fazer. "
+                texto: "Pergunta para um menino no corredor.",
+                afirmacao: "O menino te ajuda a achar a sua sala e descobri que são da mesma turma e ele se apresenta, o seu nome é Josh.",
             },
             {
-                texto: "Isso é maravilhoso!",
-                afirmacao: "Quis saber como usar IA no seu dia a dia."
+                texto: "Você tenta achar sozinha.",
+                afirmacao: "Você demora para achar sua sala e leva um esporro do professor.",
             }
         ]
     },
     {
-        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
+        enunciado: "É intervalo. Você precisa de um lugar pra se sentar. Com quem você senta?",
         alternativas: [
             {
-                texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
-                afirmacao: "Conseguiu utilizar a IA para buscar informações úteis."
+                texto: "Uma menina comendo sozinha.",
+                afirmacao: "A menina que você conheceu é legal e simpática, vocês viram amigas.",
             },
             {
-                texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
-                afirmacao: "Sentiu mais facilidade em utilizar seus próprios recursos para escrever seu trabalho."
+                texto: "Grupo de meninas.",
+                afirmacao: "O grupo de meninas te estranham e te excluem. Você fica com fama de esquisita entre elas.",
             }
         ]
     },
     {
-        enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        enunciado: "Passou um tempo e você se acostumou a sua nova escola. Agora o professor deu um trabalho em duplas e já montou os pares, e você fica com o Josh. Como você reagiu? ",
         alternativas: [
             {
-                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
-                afirmacao: "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA."
+                texto: "Você vai falar com ele.",
+                afirmacao: "Sobre o trabalho que o professor passou, você tem a iniciativa de ir lá falar com Josh (sua dupla) sobre o trabalho dado e você sugere de fazerem o trabalho na casa dele, e ele concorda. Você não lembrava dele ser tão gentil e bonito.",
             },
             {
-                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
-                afirmacao: "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética."
+                texto: "Você espera ele vir falar com você primeiro.",
+                afirmacao: "Sobre o trabalho que o professor passou, você esperou Josh vir falar com você. Ele acha que você é bem tímida e vem falar com você. Marcaram de fazer o trabalho na casa dele e ele acha você muito quieta.",
             }
         ]
     },
     {
-        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+        enunciado: "É a data marcada. Você vai até a casa de Josh e fazem o trabalho. Após terminarem, decidem assistir um filme. Você percebe que ele não para de te encara. Como você reage?",
         alternativas: [
             {
-                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
-                afirmacao: "Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes."
+                texto: "Você tenta ignorar.",
+                afirmacao: "Na casa de Josh, após fazer o trabalho, ele continua te encarando. Você decide tentar ignorar e ele continua te encarando e tenta puxar um assunto e vocês começam a conversar. Você se sente atraida por ele e se ele teria algum interesse em você.",
             },
             {
-                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
-                afirmacao: "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!"
+                texto: "O encara de volta na mesma intensidade.",
+                afirmacao: "Na casa de Josh, após fazer o trabalho, ele continua te encarando. Você decide encarar de volta... Vocês se encaram por uns segundos e sentem a química. Ele se aproxima e vocês se beijam. Você fica preocupada que ele só te beijou por impulso enqunato você se sente atraida por ele de verdade.",
             }
         ]
     },
     {
-        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
+        enunciado: "Eu gostaria de te perguntar algo, mas tenho medo de você me achar estranho.' Josh diz olhando para você. 'Pode perguntar' Você responde. 'Desde que te vi no corredor percebi o quão bonita você é. E depois de conversar com você, reparei que você também é muito legal. Quer ir num encontro comigo?' Ele diz tímido. O que você faz? ",
         alternativas: [
             {
-                texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
-                afirmacao: "Infelizmente passou a utilizar a IA para fazer todas suas tarefas e agora se sente dependente da IA para tudo."
+                texto: "Você fica nervosa e enrola ele.",
+                afirmacao: "Josh te chama para sair e você o enrola. Ele fica confuso e você não o responde por um tempo. Ele acaba perdendo o interesse.",
             },
             {
-                texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
-                afirmacao: "Percebeu que toda IA reproduz orientações baseadas na empresa que programou e muito do que o chat escrevia não refletia o que pensava e por isso sabe que os textos gerados pela IA devem servir como auxílio e não resultado final. "
+                texto: "Você aceita.",
+                afirmacao: "Ele te chama para sair e você aceita. Josh fica contente e marcam o encontro. Tudo ocorre bem e vocês começam um relacionamento!",
             }
         ]
     },
@@ -105,7 +105,7 @@ function respostaSelecionada(opcaoSelecionada) {
 }
 
 function mostraResultado() {
-    caixaPerguntas.textContent = "Em 2049...";
+    caixaPerguntas.textContent = "";
     textoResultado.textContent = historiaFinal;
     caixaAlternativas.textContent = "";
 }
